29c24de4260b488177ffb25cb1cf5d5b6ba67056
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-24 11:23:51 +0100

    two kins of +

--- src/tpNotes.jl
+++ src/tpNotes.jl
@@ -4,7 +4,8 @@ struct People
     name::String
 end
 
-Base.:+(p1::People, p2::People) = "$(string(p1.name)) and $(string(p2.name))"
-    
+Base.:+(p1::People, p2::People) = "$(string(p1.name)) and $(string(p2.name))"    
+
+Base.length(p::People) = length(p.name) +1
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,6 @@ using Test
     p1 = tpNotes.People("Søren")
     p2 = tpNotes.People("Mette")
     @test p1 + p2 == "Søren and Mette"
+    @test "$(p1 + p2) says 2+2 is $(2 + 2)" == "Søren and Mette says 2+2 is 4"
+    @test length(p1) == 6
 end
